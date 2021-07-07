72b2d8c45793c01ce5e9854b0fd21e58262659d3
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-07-07 16:24:09 +0100

    adding DB

--- terraform/infrastructure/database.tf
+++ terraform/infrastructure/database.tf
@@ -1,55 +1,49 @@
-//resource "random_id" "database" {
-//  byte_length = 8
-//}
-//
-//resource "google_sql_database_instance" "database" {
-//  name             = "${var.database_instance_prefix}-${random_id.database.hex}"
-//  database_version = var.database_instance_version
-//  region           = var.project_region
-//
-//  settings {
-//    tier = var.database_instance_type
-//  }
-//}
-//
-//resource "google_sql_database" "database" {
-//  for_each = toset(var.database_names)
-//  name     = each.key
-//  instance = google_sql_database_instance.database.name
-//}
-//
-//resource "random_password" "default" {
-//  length           = 32
-//  special          = true
-//  override_special = "_%@"
-//}
-//
-//resource "google_sql_user" "default" {
-//  name     = "postgres"
-//  instance = google_sql_database_instance.database.name
-//  password = random_password.default.result
-//}
-//
-//resource "google_sql_user" "boundary" {
-//  name     = "boundary"
-//  instance = google_sql_database_instance.database.name
-//  password = "boundary"
-//}
-//
-//resource "google_service_account" "cloudsql_proxy" {
-//  account_id   = "cloudsql-proxy"
-//  display_name = "Cloud SQL Proxy"
-//}
-//
-//resource "google_service_account_key" "cloudsql_proxy" {
-//  service_account_id = google_service_account.cloudsql_proxy.name
-//}
-//
-//resource "google_project_iam_member" "cloudsql_proxy" {
-//  role   = "roles/cloudsql.client"
-//  member = "serviceAccount:${google_service_account.cloudsql_proxy.email}"
-//}
-//
+resource "random_id" "database" {
+  byte_length = 8
+}
+
+resource "google_sql_database_instance" "database" {
+  name             = "${var.database_instance_prefix}-${random_id.database.hex}"
+  database_version = var.database_instance_version
+  region           = var.project_region
+
+  settings {
+    tier = var.database_instance_type
+  }
+}
+
+resource "google_sql_database" "database" {
+  for_each = toset(var.database_names)
+  name     = each.key
+  instance = google_sql_database_instance.database.name
+}
+
+resource "random_password" "default" {
+  length           = 32
+  special          = true
+  override_special = "_%@"
+}
+
+resource "google_sql_user" "boundary" {
+  name     = "boundary"
+  instance = google_sql_database_instance.database.name
+  password = "boundary"
+}
+
+resource "google_service_account" "cloudsql_proxy" {
+  account_id   = "cloudsql-proxy"
+  display_name = "Cloud SQL Proxy"
+}
+
+resource "google_service_account_key" "cloudsql_proxy" {
+  service_account_id = google_service_account.cloudsql_proxy.name
+}
+
+resource "google_project_iam_member" "cloudsql_proxy" {
+  role   = "roles/cloudsql.client"
+  member = "serviceAccount:${google_service_account.cloudsql_proxy.email}"
+}
+
 //resource "nomad_job" "cloudsql" {
 //  hcl2 {
 //    enabled = true
@@ -62,23 +56,23 @@
 //
 //  jobspec = file("${path.module}/files/nomad/cloudsql.nomad")
 //}
-//
-//output "database_user" {
-//  value     = google_sql_user.default.name
-//  sensitive = true
-//}
-//
-//output "database_password" {
-//  value     = google_sql_user.default.password
-//  sensitive = true
-//}
-//
-//output "database_host" {
-//  value     = "${var.project_id}:${var.project_region}:${google_sql_database_instance.database.name}"
-//  sensitive = true
-//}
-//
-//output "cloudsql_credentials" {
-//  value     = base64decode(google_service_account_key.cloudsql_proxy.private_key)
-//  sensitive = true
-//}
+
+output "database_user" {
+  value     = google_sql_user.boundary.name
+  sensitive = true
+}
+
+output "database_password" {
+  value     = google_sql_user.boundary.password
+  sensitive = true
+}
+
+output "database_host" {
+  value     = "${var.project_id}:${var.project_region}:${google_sql_database_instance.database.name}"
+  sensitive = true
+}
+
+output "cloudsql_credentials" {
+  value     = base64decode(google_service_account_key.cloudsql_proxy.private_key)
+  sensitive = true
+}
