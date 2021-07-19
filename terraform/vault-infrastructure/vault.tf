0834a8c1475b825b96bfefc5404b4a3f23d8d71e
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-07-19 11:30:08 +0100

    renaming KMS components

--- terraform/vault-infrastructure/vault.tf
+++ terraform/vault-infrastructure/vault.tf
@@ -79,4 +79,6 @@ module "vault" {
   source         = "terraform-google-modules/vault/google"
   project_id     = var.project_id
   region         = var.project_region
+  kms_keyring    = "hashicorp-vault"
+  kms_crypto_key = "vault-init"
 }
