6f79a549eeb1366c0b0fafd1345d048154c29e1b
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-08-05 16:03:08 +0100

    removing tfc agent for testing

--- terraform/1-control-plane/outputs.tf
+++ terraform/1-control-plane/outputs.tf
@@ -1,15 +1,15 @@
-output "project_id" {
-  value = data.google_project.project.id
-}
-
-output "instance_names" {
-  value = google_compute_instance.tfc-agent.*.name
-}
-
-output "terraform-dev-role" {
-  value = google_service_account.terraform-dev-role.email
-}
-
-output "tfc-agent-sa" {
-  value = google_service_account.tfc-agent.email
-}
+//output "project_id" {
+//  value = data.google_project.project.id
+//}
+//
+//output "instance_names" {
+//  value = google_compute_instance.tfc-agent.*.name
+//}
+//
+//output "terraform-dev-role" {
+//  value = google_service_account.terraform-dev-role.email
+//}
+//
+//output "tfc-agent-sa" {
+//  value = google_service_account.tfc-agent.email
+//}
