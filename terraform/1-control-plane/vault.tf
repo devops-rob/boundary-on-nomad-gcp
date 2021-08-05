e6515459b5746b3bce4998567687da8ee87edc58
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-08-05 16:04:51 +0100

    removing tfc agent for testing

--- terraform/1-control-plane/vault.tf
+++ terraform/1-control-plane/vault.tf
@@ -1,19 +1,19 @@
-//module "vault" {
-//  source         = "terraform-google-modules/vault/google"
-//  project_id     = var.project_id
-//  region         = var.project_region
-//  kms_keyring    = "hashicorp-vault"
-//  kms_crypto_key = "vault-init"
-//  allow_ssh      = false
-//
-//  vault_allowed_cidrs = var.allow_list
-//}
-//
-//output "vault_addr" {
-//  value = module.vault.vault_addr
-//}
-//
-//output "vault_ca_cert" {
-//  value     = module.vault.ca_cert_pem[0]
-//  sensitive = true
-//}
+module "vault" {
+  source         = "terraform-google-modules/vault/google"
+  project_id     = var.project_id
+  region         = var.project_region
+  kms_keyring    = "hashicorp-vault"
+  kms_crypto_key = "vault-init"
+  allow_ssh      = false
+
+  vault_allowed_cidrs = var.allow_list
+}
+
+output "vault_addr" {
+  value = module.vault.vault_addr
+}
+
+output "vault_ca_cert" {
+  value     = module.vault.ca_cert_pem[0]
+  sensitive = true
+}
