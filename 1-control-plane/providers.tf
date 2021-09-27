0137629fb9d61202029f1ed3f0dd8b7506919c77
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-09-27 11:25:12 +0100

    nomad provider update

--- 1-control-plane/providers.tf
+++ 1-control-plane/providers.tf
@@ -15,3 +15,9 @@ provider "google" {
 data "google_project" "project" {}
 
 provider "random" {}
+
+provider "nomad" {
+  address      = "http://${google_compute_forwarding_rule.nomad_server_internal.ip_address}:4646"
+  secret_id    = data.consul_acl_token_secret_id.nomad_server.secret_id
+  consul_token = var.consul_master_token
+}
