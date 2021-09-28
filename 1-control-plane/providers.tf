0cc283abbc4042f8c022f6f68b856f8558f625fc
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-09-28 15:08:49 +0100

    removing db

--- 1-control-plane/providers.tf
+++ 1-control-plane/providers.tf
@@ -22,7 +22,7 @@ provider "random" {}
 #   consul_token = var.consul_master_token
 # }
 
-# provider "consul" {
-#   address = "http://${google_compute_forwarding_rule.consul_server_internal.ip_address}:8500"
-#   token   = var.consul_master_token
-# }
+provider "consul" {
+  address = "http://${google_compute_forwarding_rule.consul_server_internal.ip_address}:8500"
+  token   = var.consul_master_token
+}
