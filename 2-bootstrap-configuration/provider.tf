64de908b9e05f059da0a7d364c44d1076edfc96f
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-09-29 11:11:46 +0100

    application config

--- 2-bootstrap-configuration/provider.tf
+++ 2-bootstrap-configuration/provider.tf
@@ -16,13 +16,21 @@ data "google_project" "project" {}
 
 provider "random" {}
 
+data "google_compute_forwarding_rule" "nomad" {
+    name = "nomad-server-internal-forwarding-rule"
+}
+
+data "google_compute_forwarding_rule" "consul" {
+    name = "consul-server-internal-forwarding-rule"
+}
+
 provider "nomad" {
-  address      = "http://${google_compute_forwarding_rule.nomad_server_internal.ip_address}:4646"
+  address      = "http://${data.google_compute_forwarding_rule.nomad.ip_address}:4646"
   secret_id    = data.consul_acl_token_secret_id.nomad_server.secret_id
   consul_token = var.consul_master_token
 }
 
 provider "consul" {
-  address = "http://${google_compute_forwarding_rule.consul_server_internal.ip_address}:8500"
+  address = "http://${data.google_compute_forwarding_rule.consul.ip_address}:8500"
   token   = var.consul_master_token
 }
